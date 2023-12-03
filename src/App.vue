f32d6e5545add544742ab94e11fe16f547593e74
John *Seg* Seggerson <seg@spacebetweenstudios.com>
2023-12-02 17:23:12 -0800

    Fixes
    * Fix center positioning of content.
    * Trim down Bootstrap imports.
    * Fix OG Image reference now that we have a full URL.

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <div class="container">
-    <div class="row justify-content-center align-items-center w-100 vh-100">
+  <div class="container h-100">
+    <div class="row justify-content-center align-items-center h-100">
       <div class="col-auto display-1">
         🎮 🧑‍💻 🎠
       </div>
