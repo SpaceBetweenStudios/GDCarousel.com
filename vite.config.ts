0f43e4d8518c09646132466b04db97e5d83c5217
John *Seg* Seggerson <seg@segonmedia.com>
2025-01-06 18:07:59 -0500

    SASS -> SASS Embedded
    * Move to SASS Embedded
    * Ignore warnings for now (because Bootstrap).

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         api: "legacy",
         silenceDeprecations: [
           "legacy-js-api",
+          "global-builtin",
           "import",
         ],
       }
